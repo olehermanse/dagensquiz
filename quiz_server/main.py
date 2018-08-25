f4bfc0016a3c2e12ced91316992ca4538ce85bdb
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2018-08-25 16:52:07 -0700

    English UI
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- quiz_server/main.py
+++ quiz_server/main.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import random
 
@@ -11,6 +12,27 @@ from quiz_server.quiz import get_quiz, init
 app = Flask(__name__, static_url_path='', static_folder='static')
 Compress(app)
 
+NORWEGIAN = {
+    "title": "Dagens Quiz",
+    "url": "https://dagensquiz.no/",
+    "more": "Mer quiz...",
+    "solution": "Dagens Fasit",
+    "id": "no"
+}
+
+ENGLISH = {
+    "title": "Daily Quiz",
+    "url": "https://dailyquiz.app/",
+    "more": "More quiz...",
+    "solution": "Solution",
+    "id": "en"
+}
+
+LANG = NORWEGIAN
+
+if "ENGLISH_QUIZ" in os.environ and os.environ["ENGLISH_QUIZ"] == "1":
+    LANG = ENGLISH
+
 
 # Web server:
 @app.route('/')
@@ -22,16 +44,24 @@ def root():
 
 @app.route('/<string:quiz_id>', methods=["GET"])
 def editor(quiz_id):
+    global LANG
     if "favicon.ico" == quiz_id:
         abort(404)
     if quiz_id == "random":
-        return redirect("/{}".format(random.randint(0,1000)))
+        return redirect("/{}".format(random.randint(0, 1000)))
     questions, answers = get_quiz(quiz_id)
     subtitle = quiz_id
     if "-" not in subtitle:
-        subtitle = "#"+subtitle
+        subtitle = "#" + subtitle
     return render_template(
-        'index.html', date=subtitle, questions=questions, answers=answers)
+        'index.html',
+        text_title=LANG["title"],
+        text_solution=LANG["solution"],
+        text_more=LANG["more"],
+        url=LANG["url"],
+        date=subtitle,
+        questions=questions,
+        answers=answers)
 
 
 def get_args():
@@ -59,7 +89,8 @@ def main():
         app.config['SERVER_NAME'] = "127.0.0.1:5000"
         app.config['DEBUG'] = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-    init()
+    global LANG
+    init(lang=LANG["id"])
     start_server(args.ip, args.port)
 
 
--- quiz_server/quiz.py
+++ quiz_server/quiz.py
@@ -1,4 +1,3 @@
-
 import json
 from collections import OrderedDict
 
@@ -8,10 +7,12 @@ quiz_count = 0
 quizzes = {}
 quiz_data = {}
 
+
 def read_json(path):
     with open(path, "r", encoding='utf-8') as f:
         d = json.loads(f.read(), object_pairs_hook=OrderedDict)
     return d
+
 
 def read_questions(path):
     with open(path, "r", encoding='utf-8') as f:
@@ -19,34 +20,39 @@ def read_questions(path):
     questions = []
     answers = []
     for line in lines:
-        q,a = line.split("    - ")
+        q, a = line.split("    - ")
         questions.append(q.strip())
         answers.append(a.strip())
     return questions, answers
 
-def fill_category(category):
-    filename = category["questions"]
-    assert type(filename) is str
-    questions, answers = read_questions("2018/no/" + filename)
-    category["questions"] = []
-    for q,a in zip(questions, answers):
-        obj = {}
-        obj["question"] = q
-        obj["answer"] = a
-        category["questions"].append(obj)
+
+def fill_category(category, lang):
+    if "questions" in category:
+        filename = category["questions"]
+        assert type(filename) is str
+        questions, answers = read_questions("2018/{}/{}".format(
+            lang, filename))
+        category["questions"] = []
+        for q, a in zip(questions, answers):
+            obj = {}
+            obj["question"] = q
+            obj["answer"] = a
+            category["questions"].append(obj)
 
     if "subcategories" in category:
         subcategories = category["subcategories"]
         for sub in subcategories:
-            fill_category(sub)
+            fill_category(sub, lang)
 
-def init():
-    structure = read_json("2018/no/data.json")
+
+def init(lang):
+    structure = read_json("2018/{}/data.json".format(lang))
     categories = structure["categories"]
     for cat in categories:
-        fill_category(cat)
+        fill_category(cat, lang)
     global quiz_data
     quiz_data = structure
+
 
 def pick_question(category, n, seed):
     questions = category["questions"]
@@ -54,8 +60,10 @@ def pick_question(category, n, seed):
         return None
     return pick(questions, seed, n)
 
+
 def pretty(data):
     return json.dumps(data, indent=2)
+
 
 def gen_quiz(seed, length=10):
     state = (length + seed) // 2
@@ -74,13 +82,15 @@ def gen_quiz(seed, length=10):
         question_categories.append(category)
         state += 1
     for category in question_categories:
-        q,a = None, None
+        if "questions" not in category:
+            continue
+        q, a = None, None
         obj = None
         while q is None or a is None or q in questions or a in answers:
             obj = pick_question(category, seed, state)
             if not obj:
                 continue
-            q,a = obj["question"], obj["answer"]
+            q, a = obj["question"], obj["answer"]
             state += 1
         questions.append(q)
         answers.append(a)
@@ -89,6 +99,7 @@ def gen_quiz(seed, length=10):
     qa = qa[0:length]
     questions, answers = zip(*qa)
     return [questions, answers]
+
 
 def get_quiz(quiz_id):
     seed = hash(quiz_id)
